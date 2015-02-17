2eccedcc842a47d38df08d917a4c156e17900b0f
Travis Baer <tbaer@clockwork-solutions.com>
2015-02-17 17:34:10 -0600

    looked at a few more packages, removed duplicates and out-of-bounds from input data

--- contours.R
+++ contours.R
@@ -2,9 +2,15 @@
 # February 2015
 
 require(dplyr)
-require(lattice)
+require(lattice);require(latticeExtra)
+require(spatstat)
+require(spdep)
+require(GeoXp)
+require(ggplot2)
+require(tripack)
+require(deldir)
 
-# what's the right function?
+# what's the right function? for plotting, for interpolating between points
 
 
 # try filled.conour
@@ -46,6 +52,8 @@ filled.contour(cx,cy,cz)
 #### 
 
 # try contour/level plot from lattice
+# level plots place a box around each point the same color as the point
+#     box sizes are as small as the two closest data points, so there is high potential for sparsity (no color)
 x <- seq(pi/4, 5 * pi, length.out = 100)
 y <- seq(pi/4, 5 * pi, length.out = 100)
 r <- as.vector(sqrt(outer(x^2, y^2, "+")))
@@ -58,35 +66,73 @@ levelplot(z~x*y, grid, cuts = 50, scales=list(log="e"), xlab="",
 grid$z[sample(seq(length(grid$z)),floor(length(grid$z)*0.05))]<-NA
 levelplot(z~x*y,grid,cuts=50,scales=list(log="e"))
 
-setwd("C:/Users/tmbae_000/Desktop/programming/contours")
+#setwd("C:/Users/tmbae_000/Desktop/programming/contours")
+setwd("c:/users/tbaer/desktop/product_development/kaggle/Rcode")
 imbal<-read.csv("MaintSupport.csv")
 # the data is a data frame - make into a matrix
-with(imbal[which(imbal$GeoLoc=="Okinawa"),],levelplot(UtilDiff~OrgPerc*NumPend))
+with(imbal[which(imbal$GeoLoc=="Pendleton"),],levelplot(UtilDiff~OrgPerc*NumPend))
 # well that sucks
 # try the strategy here: locally weighted regression
 # http://www.wekaleamstudios.co.uk/posts/displaying-data-using-level-plots/
 
 # fit separate loess for each Geographic Location
-OkiLwr1<-loess(UtilDiff~OrgPerc*NumPend,data=imbal[which(imbal$GeoLoc=="Okinawa"),],degree=1,span=0.25) # span controls smoothing - default is 0.75
-OkiLwr2<-loess(UtilDiff~OrgPerc*NumPend,data=imbal[which(imbal$GeoLoc=="Okinawa"),],degree=2,span=0.25)
-OkiConPts<-expand.grid(list("OrgPerc"=seq(min(filter(imbal,GeoLoc=="Okinawa")$OrgPerc),max(filter(imbal,GeoLoc=="Okinawa")$OrgPerc),
-                               (max(filter(imbal,GeoLoc=="Okinawa")$OrgPerc)-min(filter(imbal,GeoLoc=="Okinawa")$OrgPerc))/200),
-                         "NumPend"=seq(min(filter(imbal,GeoLoc=="Okinawa")$NumPend),max(filter(imbal,GeoLoc=="Okinawa")$NumPend),
-                               (max(filter(imbal,GeoLoc=="Okinawa")$NumPend)-min(filter(imbal,GeoLoc=="Okinawa")$NumPend))/200))) # 201 on each axis 
+PenLwr1<-loess(UtilDiff~OrgPerc*NumPend,data=imbal[which(imbal$GeoLoc=="Pendleton"),],degree=1,span=0.25) # span controls smoothing - default is 0.75
+PenLwr2<-loess(UtilDiff~OrgPerc*NumPend,data=imbal[which(imbal$GeoLoc=="Pendleton"),],degree=2,span=0.25)
+PenConPts<-expand.grid(list("OrgPerc"=seq(min(filter(imbal,GeoLoc=="Pendleton")$OrgPerc),max(filter(imbal,GeoLoc=="Pendleton")$OrgPerc),
+                               (max(filter(imbal,GeoLoc=="Pendleton")$OrgPerc)-min(filter(imbal,GeoLoc=="Pendleton")$OrgPerc))/200),
+                         "NumPend"=seq(min(filter(imbal,GeoLoc=="Pendleton")$NumPend),max(filter(imbal,GeoLoc=="Pendleton")$NumPend),
+                               (max(filter(imbal,GeoLoc=="Pendleton")$NumPend)-min(filter(imbal,GeoLoc=="Pendleton")$NumPend))/200))) # 201 on each axis 
 # predict the z at each and attach to Points for Plotting
-Okiz1<-predict(OkiLwr1,newdata=OkiConPts)
-OkiConPts$Okiz1<-as.numeric(Okiz1)
-Okiz2<-predict(OkiLwr2,newdata=OkiConPts)
-OkiConPts$Okiz2<-as.numeric(Okiz2)
+Penz1<-predict(PenLwr1,newdata=PenConPts)
+PenConPts$Penz1<-as.numeric(Penz1)
+Penz2<-predict(PenLwr2,newdata=PenConPts)
+PenConPts$Penz2<-as.numeric(Penz2)
 
 # now try the lattice levelplot again
-levelplot(Okiz1~OrgPerc*NumPend,data=OkiConPts) # first order
-levelplot(Okiz2~OrgPerc*NumPend,data=OkiConPts) # second order
+levelplot(Penz1~OrgPerc*NumPend,data=PenConPts) # first order
+p1<-levelplot(Penz2~OrgPerc*NumPend,data=PenConPts) # second order
+#levelplot(Penz2~OrgPerc*NumPend,data=PenConPts, at=c(0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1)) # explict contours
+p1 + xyplot(PenConPts$NumPend~PenConPts$OrgPerc,cex=.5) # der
+# plot the points using another panel function ?
+levelplot(Penz2~OrgPerc*NumPend,data=PenConPts, at=c(0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1),
+          panel = function(x,y,z){
+          panel.levelplot(x,y,z,subscripts=seq(length(z)))
+          panel.points(x,y)
+          }) # unsure here
 
+# try simple example here:
+tx<-c(1,1,2,2,3,3) # c(1,1,3,3)
+ty<-c(1,2,1,3.5,1,3) # c(1,2,1,3)
+tz<-c(1,2,2.5,4,3,4) # c(1,2,3,4)
+levelplot(tz~tx*ty) # boring - no interpolation between points
+# try interpolation
+simpleLWR1<-loess(tz~tx*ty,degree=2,span=2) # span controls smoothing - default is 0.75
+tConPts<-expand.grid(list(tx=seq(1,3,((3-1)/20)),ty=seq(1,3,((3-1)/20))))
+tz1<-predict(simpleLWR1,newdata=tConPts)
+# it seems loess has a difficult time with only four points - it has the same problem with 6
 
-
+# try ggplot2
+g1 <- ggplot(data.frame(tx,ty,tz),aes(tx,ty,fill=tz))
+g1 + geom_tile() # gives the same boring plot with two points missing and no interpolation
+g2<-ggplot(PenConPts, aes(x=OrgPerc,y=NumPend,fill=Penz2))
+g2 + geom_tile()
+# eliminate parts of contour outside the boundaries
 # find minimum and maximum % org for each Pendleton
 imbalBound<-data.frame(group_by(imbal,NumPend) %>% summarise("minOrgP"=min(OrgPerc),"maxorgp"=max(OrgPerc)))
+
+
+#### try GeoXp and spdep
+abc<-tri2nb()
+
+##### Make Delaunay triangles with the tripack package
+# first three data points must not be collinear
+randomorder<-sample(length(imbal[imbal$GeoLoc=="Pendleton","OrgPerc"]))
+edgesPend<-tri.mesh(x=imbal[imbal$GeoLoc=="Pendleton","OrgPerc"][randomorder],
+                    y=imbal[imbal$GeoLoc=="Pendleton","NumPend"][randomorder])
+##### Make Delaunay triangles with the deldir package
+edgesPendDD<-deldir(x=imbal[imbal$GeoLoc=="Pendleton","OrgPerc"][randomorder],   ### these two methods give the same connections - maybe JMP 9 is wrong or isn't using Delaunay triangles
+                    y=imbal[imbal$GeoLoc=="Pendleton","NumPend"][randomorder],
+                    plotit=TRUE)
 
 # other notes
 # use expand.grid to build out a matrix
